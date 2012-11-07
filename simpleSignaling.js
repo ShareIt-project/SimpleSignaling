69bcde5b0ff3559a6e9f7de7741ac07c2cbb453a
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2012-11-07 20:11:31 +0100

    Adde websocket.onopen

--- simpleSignaling.js
+++ simpleSignaling.js
@@ -2,27 +2,29 @@ function SimpleSignaling(configuration)
 {
     var self = this;
 
-    var websocket = WebSocket(configuration.ws_uri)
+    var websocket = WebSocket(configuration.ws_uri);
+        websocket.open = function()
+        {
+            // Message received
+            websocket.onmessage = function(message)
+            {
+                message = JSON.parse(message.data);
+
+                var uid  = message[0];
+                var data = message[1];
+
+                if(self.onmessage)
+                   self.onmessage(uid, data);
+            };
+        };
 
     // Compose and send message
-    this.emit = function(uid, data)
+    this.send = function(uid, data)
     {
         websocket.send(JSON.stringify([uid, data]), function(error)
         {
             if(error)
                 console.warning(error);
         });
-    }
-
-    // Message received
-    websocket.onmessage = function(message)
-    {
-        message = JSON.parse(message.data);
-
-        var uid  = message[0];
-        var data = message[1];
-
-        if(self.onmessage)
-           self.onmessage(uid, data);
     };
 }
