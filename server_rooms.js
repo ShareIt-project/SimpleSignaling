b0ac68e34e801c949f718910edd51d90a33b8617
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2012-12-08 22:00:55 +0100

    Added missing semicolons

--- server_rooms.js
+++ server_rooms.js
@@ -3,53 +3,53 @@ var fs = require('fs');
 
 var options = {key:  fs.readFileSync('certs/privatekey.pem').toString(),
 			   cert: fs.readFileSync('certs/certificate.pem').toString(),
-			   ca:   [fs.readFileSync('certs/certrequest.csr').toString()]}
+			   ca:   [fs.readFileSync('certs/certrequest.csr').toString()]};
 
 // Get AppFog port, or set 8001 as default one
-var port = process.env.VMC_APP_PORT || 8001
+var port = process.env.VMC_APP_PORT || 8001;
 
 // HTTP server
 function requestListener(req, res)
 {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('This is a ShareIt! handshake server. You can get a copy of the ')
-  res.end('source code at <a href="https://github.com/piranna/ShareIt">GitHub</a>')
+  res.write('This is a ShareIt! handshake server. You can get a copy of the ');
+  res.end('source code at <a href="https://github.com/piranna/ShareIt">GitHub</a>');
 }
 
-var server = require('http').createServer(requestListener)
-//var server = require('https').createServer(options, requestListener)
+var server = require('http').createServer(requestListener);
+//var server = require('https').createServer(options, requestListener);
     server.listen(port);
 
 // Handshake server
-var WebSocketServer = require('ws').Server
-var wss = new WebSocketServer({server: server})
+var WebSocketServer = require('ws').Server;
+var wss = new WebSocketServer({server: server});
 
 // Dict to store rooms
-wss.rooms = {}
+wss.rooms = {};
 
 wss.on('connection', function(socket)
 {
     socket.onmessage = function(message)
     {
-        var args = JSON.parse(message.data)
+        var args = JSON.parse(message.data);
 
-        var eventName = args[0]
-        var roomId    = args[1]
+        var eventName = args[0];
+        var roomId    = args[1];
 
-        var room = wss.rooms[roomId]
+        var room = wss.rooms[roomId];
         if(!room)
         {
-            room = wss.rooms[roomId] = []
-            room.id = roomId
+            room = wss.rooms[roomId] = [];
+            room.id = roomId;
         }
 
-        var len = room.length
+        var len = room.length;
 
         if(len >= 2)
-            socket.send(JSON.stringify(['joiner.error', 'room full']))
+            socket.send(JSON.stringify(['joiner.error', 'room full']));
         else
         {
-            room.append(socket)
+            room.append(socket);
             socket.room = room;
 
             socket.send(JSON.stringify(['joiner.success']));
@@ -64,12 +64,12 @@ wss.on('connection', function(socket)
                     socket.peer.peer = socket;
 
                     // Notify to both peers that we are now connected
-                    socket.send(JSON.stringify(['peer.connected', socket.id]))
-                    socket.peer.send(JSON.stringify(['peer.connected', socket.id]))
+                    socket.send(JSON.stringify(['peer.connected', socket.id]));
+                    socket.peer.send(JSON.stringify(['peer.connected', socket.id]));
                 }
             }
         }
-    }
+    };
 
 	socket.onclose = function()
 	{
@@ -80,11 +80,11 @@ wss.on('connection', function(socket)
 			socket.peer.peer = undefined;
 		}
 
-        var roomId = socket.room.id
+        var roomId = socket.room.id;
 
-        rooms.splice(rooms.indexOf(socket), 1)
+        rooms.splice(rooms.indexOf(socket), 1);
 
         if(!rooms[roomId].lenght)
-            delete rooms[roomId]
-	}
-})
+            delete rooms[roomId];
+	};
+});
